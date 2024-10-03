6793af51070d05a654740ef99efde4f04346facf
Tyron <lucena.tyronscott@gmail.com>
2024-10-03 08:22:42 +0800

    chore: add comments

--- main.cpp
+++ main.cpp
@@ -5,16 +5,14 @@
 
 using namespace std;
 
-
-
-
-
-
-
 int main() {
+    // set locale to support unicode characters
     setlocale(LC_ALL, "");
 
+    // the main inventory object, manages the items
     const InventoryImpl inventory_impl;
+
+    // the user interface object that displays the inventory contents
     InventoryUi ui(inventory_impl);
     ui.display();
 }
